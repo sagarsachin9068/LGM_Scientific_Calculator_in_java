49334e8503b7781caf52585ac63eff0eb569ead0
sachin sagar <96176699+sagarsachin9068@users.noreply.github.com>
2023-06-22 17:40:18 +0530

    Add files via upload

---
+++ CalcLogic.java
@@ -0,0 +1,116 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/*
+ @auther sachin sagar  
+ @since 20/06/2023
+ */
+public class CalcLogic {
+	// -- Instance variables.
+	// The current total is all we need to remember.
+	private double StoredVal = 0.0;
+	/* script engine manager array */
+	ScriptEngineManager mgr = new ScriptEngineManager();
+	ScriptEngine engine = mgr.getEngineByName("JavaScript");
+	/**
+	 * 
+	 * @sachin sum to be evaluated 
+	 * takes in the result then parses the result into the script engine to be evaluated and then 
+	 * if the result is == to a type double out of  bounds error that they tried to divide by zero and throws the DivideByZeroException
+	 * and returns an error 
+	 * else takes the ScriptException and returns error 
+	 * @throws DivideByZeroException checks that you havent divided by 0
+	 * @return sum
+	 */
+	
+	public String Evaluate(String sum) throws DivideByZeroException {
+		try
+
+		{
+			double result = Double.parseDouble(engine.eval(sum) + "");
+
+			if (result == Double.POSITIVE_INFINITY) {
+				throw new DivideByZeroException();
+			} else {
+				return engine.eval(sum) + "";
+			}
+		} catch (ScriptException e) {
+			return "ERROR";
+		}
+
+	}
+	
+
+	// -- ENDS CONVERT ALL THE STRINGS ENTER ON THE PAD TO NUMERIC DOUBLES-------------------------------------------*/
+	// -----------------BEGINS EXTRA FUNCTIONS FOR THE CALCULATOR-------------------------------------------*/
+    public String Square(String n) {
+		double square = Double.parseDouble(n);
+		square = Math.pow(square, 2);
+		return Double.toString(square);
+	}
+	/**
+	 * 
+	 * @sachin n return to its square root
+	 * takes in n turns it into a double from a string  
+	 *gets the square root of n using Math.sqrt(square) so it takes the inputed number and gets the sqaure root
+	 *then reparses the double to a string and then returns it to a string
+	 *@return SquareRoot 
+	 */
+	public String SquareRoot(String n) {
+		double square = Double.parseDouble(n);
+		square = Math.sqrt(square);
+		return Double.toString(square);
+	}
+
+	// -----------------ENDS EXTRA FUNCTIONS FOR THE CALCULATOR-------------------------------------------*/
+	// ----------------------------------MEMORY FUNCTIONS FOR THE CALCULATORSTARTS --------------------------------------------------------///////////////////////////////////////////////////
+	/**
+	 * 
+	 * @sachin displayText stores the value 
+	 * takes in displayText turns it into a double from a string  
+	 *gets the total on the displayText and saves it
+	 *then reparses the double to a string and then returns it to a string
+	 *
+	 */
+	public void Store(String displayText) {
+		StoredVal = Double.parseDouble(displayText);
+	}
+	/**
+	 * 
+	 * sends out the StoredVal thats saved and then prints it on screen  
+	 *@return StoredVal
+	 *
+	 */
+	public double GetStoredVaL() {
+		return StoredVal;
+	}
+	/**
+	 * 
+	 * resets the StoredVal to 0.0
+	 *
+	 */
+	public void ResetStoredVal() {
+		StoredVal = 0.0;
+	}
+	/**
+	 *@sachin n 
+	 * adds to  StoredVal 
+	 *
+	 */
+	public void PlusStoredVal(double n) {
+		StoredVal += n;
+	}
+	/**
+	 *@sachin n 
+	 * subtracts from StoredVal 
+	 *
+	 *
+	 */
+	public void MinusStoredVal(double n) {
+		StoredVal -= n;
+	}
+	/// -----------------------------MEMORY FUNTIONS FOR THE CALCULATOR
+	/// ENDS-------------------------------------------------------------
+
+}
